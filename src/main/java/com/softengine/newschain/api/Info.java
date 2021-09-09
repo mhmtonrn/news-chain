b4353773f24fe353c2450705967379c681b77e22
mhmtonrn <mhmtonar1@gmail.com>
2021-09-10 00:42:02 +0300

    Update Info.java

--- src/main/java/com/softengine/newschain/api/Info.java
+++ src/main/java/com/softengine/newschain/api/Info.java
@@ -4,12 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 @RequestMapping("api/info")
 public class Info {
 
     @GetMapping
     public String info(){
-        return "news-chain v1.1";
+        return "news-chain v1.1 -> " + LocalDateTime.now();
     }
 }
