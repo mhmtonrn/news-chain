8f29876e2ff000f3708bf34ecb6edc6533bf9cb0
mhmtonrn <mhmtonar1@gmail.com>
2021-09-18 12:43:59 +0300

    Update SwaggerConfig.java

--- src/main/java/com/softengine/newschain/configuration/SwaggerConfig.java
+++ src/main/java/com/softengine/newschain/configuration/SwaggerConfig.java
@@ -23,7 +23,7 @@ public class SwaggerConfig {
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .host("monar2.jprq.live")
+                //.host("monar2.jprq.live")
                 .select() .apis(RequestHandlerSelectors.basePackage("com.softengine.newschain"))
                 .paths(regex("/api.*"))
                 .build().apiInfo(metaData());
